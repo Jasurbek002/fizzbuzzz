51fc72c43817946aa6ec1d1b43456fc7c7071c37
unknown <jasurbekturgunov25@gmail.com>
2022-01-12 12:27:14 +0500

    tayyot

--- js/msin.js
+++ js/msin.js
@@ -6,25 +6,17 @@ var isresalt=document.getElementById('resalt')
 form.addEventListener('submit', event=>{
 event.preventDefault()
 console.log()
+let a =0
+if(isinput.value<a){
+    isresalt.textContent= 'fizz'
 
+}if(isinput.value>a){
+    isresalt.textContent= 'buzz'
 
-if(isinput.value/3){
-   isresalt.textContent='fizz'
-} 
-if(isinput.value/5){
-    isresalt.textContent='buzz'
-}if(isinput.value/15){
-    isresalt.textContent='fizzbuzz'
 }else{
-    isresalt.textContent='bu yerga faqat raqam kiritiladi...'
+    isresalt.textContent= 'fizzbuzz'
+
 }
 })
-// isresalt.textContent= kottakichik
 
-// })
-// function kottakichik 0){
-//     if(number>0)return 'fizz'
-//     if(number<0)return 'buzz'
-//     return 'fizzbuzz'
 
-// }
